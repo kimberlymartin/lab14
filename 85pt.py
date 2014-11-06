da0910497fcecb6f9180448d88f21f59e63e21b2
kimberlymartin <kimberlymartin99@gmail.com>
2014-11-06 15:22:19 -0600

    85pt

--- 85pt.py
+++ 85pt.py
@@ -28,11 +28,13 @@ class MyApp:
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="Left", background= "green")
 		self.button1.grid(row=0,column=0)
-		
-	
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Right", background="green")
+		self.button2.grid(row=0,column=1)
+	        
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
-		 
+		self.button2.bind("<Button-1>", self.button2Click)
 		  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
@@ -47,9 +49,22 @@ class MyApp:
 		global drawpad
 		global drawpadwidth
 		global drawpadheight
+		x1,x2,y1,y2 = drawpad.coords(oval)
+		if x1 >= 0:
+		    drawpad.move(oval,-5,0)
 	
 	# Add the button2Click method
-		
+	def button2Click(self,event):
+	        global oval
+	        global drawpad
+	        global drawpadwidth
+	        global drawpadheight
+	        x1,x2,y1,y2 = drawpad.coords(oval)
+	        if x2 <= 480:
+	            drawpad.move(oval,5,0)
+	        else:
+	            drawpad.move(oval,0,0)
+	           
 myapp = MyApp(root)
 
 root.mainloop()
